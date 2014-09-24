7aa2892d90031b97ceccb02d2b6e8c1818e47a80
Bruno Philipe <brunophilipe@me.com>
2014-09-24 11:25:04 -0300

    Initial Commit

---
+++ TapWheel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TapWheel
+//
+//  Created by Bruno Philipe on 9/24/14.
+//  Copyright (c) 2014 Bruno Philipe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TapWheel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TapWheel
+//
+//  Created by Bruno Philipe on 9/24/14.
+//  Copyright (c) 2014 Bruno Philipe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
