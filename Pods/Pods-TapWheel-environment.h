28072a907296f9970dd15a834612bec98ae0e906
Bruno Philipe <brunophilipe@me.com>
2014-09-27 18:53:08 -0300

    Added cocoa pods

---
+++ Pods/Pods-TapWheel-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// MarqueeLabel
+#define COCOAPODS_POD_AVAILABLE_MarqueeLabel
+#define COCOAPODS_VERSION_MAJOR_MarqueeLabel 2
+#define COCOAPODS_VERSION_MINOR_MarqueeLabel 0
+#define COCOAPODS_VERSION_PATCH_MarqueeLabel 5
+
